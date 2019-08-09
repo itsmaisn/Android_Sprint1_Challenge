0f8161be653644a916d598d131b5fa01fd81d791
itsmaisn <itsmaisn@gmail.com>
2019-08-09 08:44:44 -0700

    Initial commit

---
+++ app/src/main/java/com/lambdaschool/movielist/ui/EditPageActivity.kt
@@ -0,0 +1,12 @@
+package com.lambdaschool.movielist.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditPageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_edit_page)
+    }
+}
